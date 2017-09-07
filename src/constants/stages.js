ed1e2d6bbd6754a0ee3bc71da7fdf16407e07698
Drew Miller <poppiestar@users.noreply.github.com>
2017-09-08 00:06:47 +0100

    Fix constants so they actually work

--- src/constants/stages.js
+++ src/constants/stages.js
@@ -1,11 +1,11 @@
 // @flow
 
 const Stage: { [string]: number } = {
-    SPLASH: 1,
-    GAME_SETUP: 2,
-    GAME_ROUND: 3,
-    ENTER_SCORE: 4,
-    WINNER: 5
+    "SPLASH": 1,
+    "GAME_SETUP": 2,
+    "GAME_ROUND": 3,
+    "ENTER_SCORE": 4,
+    "WINNER": 5
 };
 
 export default Stage;
