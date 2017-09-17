6686546ef8eb1e3fb3c1bee25f260d3a458f0968
Drew Miller <poppiestar@users.noreply.github.com>
2017-09-17 23:03:45 +0100

    Display players in a list

--- src/GameRound/PlayerScores.js
+++ src/GameRound/PlayerScores.js
@@ -1,7 +1,8 @@
 // @flow
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { H1, Text, List, ListItem } from 'native-base';
 
 import type { Player, Players, Scores } from '../types';
 
@@ -16,19 +17,20 @@ const playersList = (players: { [id: number]: Player }, scores: { [id: number]: 
     Object.keys(players).reduce((list, playerId: number) => {
         const player = players[playerId];
 
-        list[playerId] = <View key={player.id}>
-            <Text>{player.name}: {reduceScores(scores[player.id])}</Text>
-        </View>;
+        list[playerId] = <ListItem key={player.id} style={{ flex: 1 }}>
+            <Text style={{ flex: 2 }}>{player.name}</Text>
+            <Text style={{ flex: 1, justifyContent: 'flex-end' }}>{reduceScores(scores[player.id])}</Text>
+        </ListItem>;
 
         return list;
     }, []);
 
 const PlayerScores = ({ players, scores }: PlayerScorePropTypes) =>
     <View>
-        <Text>Player Scores</Text>
-        <View>
+        <H1>Player Scores</H1>
+        <List>
             { playersList(players, scores) }
-        </View>
+        </List>
     </View>;
 
 export default PlayerScores;
